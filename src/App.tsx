d59ba985000dcc716f47d2e9524a7cf08b37c243
JonathanMuroma <jonathan.muroma@metropolia.fi>
2024-10-19 15:01:13 +0300

    Upload project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./styles/app.scss";
+import "./styles/userCards.scss";
+import { User } from "./utils/types";
+import useUsers from "./queries/users";
+import ListInputs from "./components/ListInputs";
+import UserCard from "./components/UserCard";
 
 function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const userData = useUsers();
+
+  const [orderStyle, setOrderStyle] = useState<boolean>(true);
+  const [filter, setFilter] = useState<string>("");
+
+  const sortByName = (userList: User[], ascOrder: boolean) => {
+    return userList.sort(function (a, b) {
+      if (a.name.toLowerCase() < b.name.toLowerCase()) return ascOrder ? -1 : 1;
+      if (a.name.toLowerCase() > b.name.toLowerCase()) return ascOrder ? 1 : -1;
+      return 0;
+    });
+  };
+
+  useEffect(() => {
+    if (userData && userData.length > 0) {
+      const sorted = sortByName(userData, true);
+      setUsers(sorted);
+    }
+  }, [userData]);
+
+  const changeOrder = () => {
+    setOrderStyle(!orderStyle);
+    const changedOrder = sortByName(users, !orderStyle);
+    setUsers(changedOrder);
+  };
+
+  const filterUsers = (val: string) => {
+    const sorted = sortByName(userData, orderStyle);
+    const filtered: User[] = sorted.filter(
+      (user) =>
+        user.name.toLowerCase().includes(val.toLowerCase()) ||
+        user.email.toLowerCase().includes(val.toLowerCase()) ||
+        user.phone.toLowerCase().includes(val.toLowerCase()) ||
+        user.website.toLowerCase().includes(val.toLowerCase()) ||
+        user.address.city.toLowerCase().includes(val.toLowerCase()) ||
+        user.address.street.toLowerCase().includes(val.toLowerCase()) ||
+        user.address.zipcode.toLowerCase().includes(val.toLowerCase())
+    );
+    setUsers(filtered);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Users:</h1>
+      <p className="description">
+        Change ordering by name through the button and filter users by their
+        data with the filter text field
+      </p>
+      {userData && userData.length > 0 ? (
+        <div>
+          <ListInputs
+            changeOrder={changeOrder}
+            orderStyle={orderStyle}
+            filter={filter}
+            setFilter={setFilter}
+            filterUsers={filterUsers}
+          />
+          <div className="user_cards_list">
+            {users.map((user) => (
+              <UserCard key={user.id} user={user} />
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="loading">
+          <p>Loading...</p>
+        </div>
+      )}
     </div>
   );
 }
